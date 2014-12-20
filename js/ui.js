7e55b05cf489a9e3f6a5c5bc68a9a4e8248bc485
Parhomenko <alex.parhomenko89@gmail.com>
2014-12-20 19:48:40 +0200

    first commit

--- js/ui.js
+++ js/ui.js
@@ -1,45 +1,56 @@
 var myInput = document.querySelector('#add');
 
 myInput.onclick = function () {
-    var newLi = document.createElement('li');
-    var newSpan = document.createElement('span');
-    var newCheck = document.createElement('input');
-    newCheck.type = 'checkbox';
-    var newButton= document.createElement('input');
-    newButton.type = 'button';
-    newButton.setAttribute('value','delete');
-    newButton.classList.add('delete');
-
-    newLi.appendChild(newSpan);
-    newLi.appendChild(newCheck);
-    newLi.appendChild(newButton);
-
     var text = document.querySelector('#message').value;
 
-    if(text != ''){
+    if (text != '') {
+        var newLi = document.createElement('li');
+        var newSpan = document.createElement('span');
+        var newCheck = document.createElement('input');
+        newCheck.type = 'checkbox';
+        var newButton = document.createElement('input');
+        newButton.type = 'button';
+        newButton.setAttribute('value', 'delete');
+        newButton.classList.add('delete');
+
+
+        newLi.appendChild(newSpan);
+        newLi.appendChild(newCheck);
+        newLi.appendChild(newButton);
+
+
+        newButton.addEventListener('click', function (event) {
+            event.target.parentNode.parentNode.removeChild(this.parentNode);
+        })
+
+
+        newCheck.addEventListener('click', function () {
+            if (newCheck.checked) {
+                newCheck.parentNode.classList.add('qqq');
+            } else {
+                newCheck.parentNode.classList.remove('qqq');
+            }
+        });
+
         newSpan.textContent = text;
         document.querySelector('.list').appendChild(newLi);
         document.querySelector('#message').value = '';
     }
 
-    newButton.addEventListener('click', function(event){
-        event.target.parentNode.parentNode.removeChild(this.parentNode);
-    })
 
 
-
-    newCheck.addEventListener('click', function(){
-        if (newCheck.checked) {
-            newCheck.classList.add('qqq');
-            newCheck.parentNode.style.textDecoration='line-through';
-        } else {
-            newCheck.classList.remove('qqq');
-            newCheck.parentNode.style.textDecoration='none';
+    document.querySelector('#delete_checked').addEventListener('click', function(){
+       var elements = document.getElementsByClassName('qqq');
+        while(elements.length > 0){
+          elements[0].parentNode.removeChild(elements[0]);
         }
     });
 
 
 
 
-};
 
+
+
+
+};
